274cfffb2ba0c24d31da17629ba17972df15ef99
ramirezsandin <ramirezsandin.developer@gmail.com>
2022-06-24 19:51:14 +0200

    Removed FC type from component

--- src/react/components/layouts/SiteLayout.tsx
+++ src/react/components/layouts/SiteLayout.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { ReactNode } from 'react'
 import Link from 'next/link'
 import { AppBar, Container, IconButton, Toolbar } from '@mui/material'
 import { Home } from '@mui/icons-material'
@@ -6,38 +6,39 @@ import { Home } from '@mui/icons-material'
 import ThemeSwitcherButton from '@/react/components/theme-switcher/ThemeSwitcherButton'
 import AlertContainer from '@/react/components/alerts/AlertContainer'
 
-const SiteLayout: FC = ({ children }) => {
-    return (
-        <>
-            <AppBar position='static'>
-                <Toolbar
-                    sx={{
-                        justifyContent: 'space-between'
-                    }}
-                >
-                    <Link href='/' passHref>
-                        <IconButton color='inherit'>
-                            <Home />
-                        </IconButton>
-                    </Link>
-                    <ThemeSwitcherButton color='inherit' />
-                </Toolbar>
-            </AppBar>
-            <Container
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    minHeight: { xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)' },
-                }}
-            >
-                {children}
-            </Container>
-            <Container component='footer'>
-
-            </Container>
-            <AlertContainer />
-        </>
-    )
+interface SiteLayoutProps {
+  children?: ReactNode
+}
+const SiteLayout = ({ children }: SiteLayoutProps) => {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar
+          sx={{
+            justifyContent: 'space-between',
+          }}
+        >
+          <Link href="/" passHref>
+            <IconButton color="inherit">
+              <Home />
+            </IconButton>
+          </Link>
+          <ThemeSwitcherButton color="inherit" />
+        </Toolbar>
+      </AppBar>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: { xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)' },
+        }}
+      >
+        {children}
+      </Container>
+      <Container component="footer"></Container>
+      <AlertContainer />
+    </>
+  )
 }
 
 export default SiteLayout
